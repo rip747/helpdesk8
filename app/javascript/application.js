8568893b59716b7fc5868ce539d36794a0767d4f
Anthony Petruzzi <karalootzkoding@gmail.com>
2025-05-08 15:17:32 +0000

    move to importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'jquery'
+import "timeago"
+
+window.$ = window.jQuery = jquery;
+console.log("Turbo loaded!");
+$(document).on("turbo:load", () => {
+    console.log("Turbo loaded!");
+});
+  
